67d96ef3c9134466d57e372890c0824911e462dc
Saprentos <saprentos@gmail.com>
2018-07-27 20:03:06 +0300

    v0.01b

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-
-let setProgress;
-
-function progress () {
-    let processBar = document.querySelector(".progressBar");
-    if (setProgress){
-        clearInterval(setProgress);
-        setProgress = null;
-    }
-    setProgress = setInterval(progressBar, 100);
-    let width = 0;
-    let widthMax = 100;
-
-     let levelUpButton = document.getElementsByClassName('upgradingButton');
-    function lvlUpButton() {
-
-    }
-       function progressBar() {
-        if (width >= widthMax) {
+    let setProgress;
+    function woodProgress() {
+        let processBar = document.querySelector(".woodProgressBar");
+        if (setProgress) {
             clearInterval(setProgress);
-        } else {
-            width += 1;
-            processBar.style.width = width + "%";
-            processBar.textContent = width + "%";
+            setProgress = null;
+        }
+        setProgress = setInterval(progressBar, 100);
+        let width = 0;
+        let widthMax = 100;
+
+        function progressBar() {
+            if (width >= widthMax) {
+                clearInterval(setProgress);
+            }
+            else {
+                width += 1;
+                processBar.style.width = width + "%";
+                processBar.textContent = width + "%";
+            }
         }
     }
-}
+
+    function stoneProgress() {
+        let processBar = document.querySelector(".stoneProgressBar");
+        if (setProgress) {
+            clearInterval(setProgress);
+            setProgress = null;
+        }
+        setProgress = setInterval(progressBar, 250);
+        let width = 0;
+        let widthMax = 100;
+
+        function progressBar() {
+            if (width >= widthMax) {
+                clearInterval(setProgress);
+            }
+            else {
+                width += 1;
+                processBar.style.width = width + "%";
+                processBar.textContent = width + "%";
+            }
+        }
+    }
