bf058492d9897ee96a275ddd3e744f608d246fb4
Saprentos <saprentos@gmail.com>
2018-07-28 14:37:40 +0300

    v0.01d

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
     let setProgress;
+function calc() {
+     let wood;
+     let stone;
+     let result;
+        wood = document.getElementById('allWood').value;
+        wood = parseInt(wood);
+        stone = document.getElementById('allStone').value;
+        stone = parseInt(stone);
+        result = wood + stone;
+        document.getElementById('allMoney').innerHTML = result;
+    }
     function woodProgress() {
         let processBar = document.querySelector(".woodProgressBar");
         if (setProgress) {
             clearInterval(setProgress);
             setProgress = null;
         }
-        setProgress = setInterval(progressBar, 100);
+        setProgress = setInterval(progressBar, 10);
         let width = 0;
         let widthMax = 100;
-
         function progressBar() {
             if (width >= widthMax) {
                 clearInterval(setProgress);
@@ -20,13 +30,14 @@
             }
         }
     }
+
     function stoneProgress() {
         let processBar = document.querySelector(".stoneProgressBar");
         if (setProgress) {
             clearInterval(setProgress);
             setProgress = null;
         }
-        setProgress = setInterval(progressBar, 250);
+        setProgress = setInterval(progressBar, 25);
         let width = 0;
         let widthMax = 100;
 
